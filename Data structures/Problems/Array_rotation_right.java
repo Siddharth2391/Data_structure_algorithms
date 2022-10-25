694abcbcafc9c32f3e7b97718895ac4e3602ed61
Siddharth Dyamgond <81026894+Siddharth2391@users.noreply.github.com>
2022-10-25 11:28:42 +0530

    Create Array_rotation_right.java

---
+++ Data structures/Problems/Array_rotation_right.java
@@ -0,0 +1,16 @@
+class Array_rotation_right {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5,6};
+        int d=4;
+        while(d-->0){
+            int temp=a[a.length-1];
+            for(int i=a.length-2;i>=0;i--){
+                a[i+1]=a[i];
+            }
+            a[0]=temp;
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
