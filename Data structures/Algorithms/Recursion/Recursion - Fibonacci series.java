eb1469eee0a276ce91ebb19edcb708ff04015892
Siddharth Dyamgond <81026894+Siddharth2391@users.noreply.github.com>
2022-10-25 21:53:41 +0530

    Create Recursion - Fibonacci series.java

---
+++ Data structures/Algorithms/Recursion/Recursion - Fibonacci series.java
@@ -0,0 +1,24 @@
+class Recursion1 {
+
+     public static long fiboc[];
+
+    public static long fibo(int n){
+        if(n<=1){
+            return n;
+        }
+        if(fiboc[n]!=0){
+            return fiboc[n];
+        }
+        long nthfib=fibo(n-1)+fibo(n-2); 
+        fiboc[n]=nthfib;
+        return nthfib;
+    }
+    public static void main(String[] args) {
+        int n=30;
+        fiboc=new long[n+1];
+        for(int i=0;i<=n;i++){
+             System.out.print(fibo(i)+" ");
+        }
+       
+    }
+}
