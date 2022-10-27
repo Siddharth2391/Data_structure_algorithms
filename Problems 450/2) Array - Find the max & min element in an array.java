e965beb37ee75550aeea5875338f343a27c34778
Siddharth Dyamgond <81026894+Siddharth2391@users.noreply.github.com>
2022-10-28 01:56:16 +0530

    Create 2) Array - Find the max & min element in an array.java

---
+++ Problems 450/2) Array - Find the max & min element in an array.java
@@ -0,0 +1,9 @@
+// Approach 1 
+import java.util.*;
+class Array {
+    public static void main(String[] args) {
+        int a[]={1,423,6,46,34,23,13,53,4}; 
+        Arrays.sort(a);
+        System.out.println("min-"+a[0]+" max-"+a[a.length-1]);
+    }
+}
