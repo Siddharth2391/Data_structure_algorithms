615a2130219f0a8a96f84290fcabe73d25c6c2dd
Siddharth Dyamgond <81026894+Siddharth2391@users.noreply.github.com>
2022-10-27 22:13:30 +0530

    Update Example1.java

--- Data structures/Algorithms/Backtracking/Example1.java
+++ Data structures/Algorithms/Backtracking/Example1.java
@@ -1,1 +1,25 @@
+import java.util.*;
 
+class Example1{
+    int a[];
+    public Example1(int n){
+        a=new int[n];
+    }
+    
+    public void  binary(int n){
+        if(n<=0){
+            System.out.println(Arrays.toString(a));
+        }else{
+            a[n-1]=0;
+            binary(n-1);
+            a[n-1]=1;
+            binary(n-1);
+        }
+    }
+    
+    public static void main(String[] args) {
+       int n=2;
+       Example1 obj=new Example1(n);
+       obj.binary(n);
+    }
+}
