b5f1a02e5b3e480ad944e46bce0b00dbeabc4323
Siddharth Dyamgond <81026894+Siddharth2391@users.noreply.github.com>
2022-10-28 16:23:25 +0530

    Update Array - Find the max & min element in an array.java

--- Problems 450/Array - Find the max & min element in an array.java
+++ Problems 450/Array - Find the max & min element in an array.java
@@ -10,3 +10,23 @@ class Array {
 
 
 //Approach 2
+import java.util.*;
+class Array {
+    public static void main(String[] args) {
+        int a[]={45,23,3,523,5,532,24,12,423};
+        int min=a[0];
+        int max=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]<min){
+                min=a[i];
+            }
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        System.out.println("Min- "+min+" Max- "+max);
+    }
+}
+
+
+//Approach 3
