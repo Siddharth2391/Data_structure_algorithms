9e7eaafef7f9cc78b96b414ea510521ea4fb9c39
siddharthDyamgond <sid.dya.rt20@rait.ac.in>
2024-12-08 22:16:31 +0530

    Character hashing

--- Algorithms/Hashing Algo/Problem2.java
+++ Algorithms/Hashing Algo/Problem2.java
@@ -1,5 +1,18 @@
 public class Problem2 {
-    public static void main(String[] args) {
 
+    // Character Hashing
+
+    public static void main(String[] args) {
+        int arr[] = new int[256];
+        String str = "Siddharth";
+
+        for (Character x : str.toCharArray()) {
+            arr[x]++;
+        }
+
+        System.out.println("S  " + arr['S']);
+        System.out.println("i " + arr['i']);
+        System.out.println("h " + arr['h']);
+        System.out.println("d " + arr['d']);
     }
 }
--- Algorithms/Hashing Algo/readme.md
+++ Algorithms/Hashing Algo/readme.md
@@ -8,3 +8,9 @@ Hashing is DSA technique which is used to stored and retireves data efficiently.
 
 1. Number Hashing
 2. Character Hashing
+
+#### Hashing methods
+
+1. Division method
+2. Mid square method
+3. Folding method
